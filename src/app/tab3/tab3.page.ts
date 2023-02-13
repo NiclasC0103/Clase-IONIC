2b462c145e9a320a71c54ab6c8d4bb3750b0e53d
S3BASCH <sebaschuquiguanga@gmail.com>
2023-02-13 09:18:51 -0500

    commit incial

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.css']
+})
+export class Tab3Page {
+
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['OK', 'CANCEL'],
+    });
+
+    await alert.present();
+  }
+}
